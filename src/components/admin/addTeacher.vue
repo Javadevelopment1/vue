05b4a7432c8998652438008b1ae329e0ac75e5ed
GitHub账号 <3260191057@qq.com>
2022-10-08 17:00:18 +0800

    1

---
+++ src/components/admin/addTeacher.vue
@@ -0,0 +1,19 @@
+<template>
+<section class="add">
+  <el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="姓名">
+    <el-input v-model="form.teacherName"></el-input>
+  </el-form-item>
+  </el-form>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
