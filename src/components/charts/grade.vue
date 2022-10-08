42320ce45936aee91582e71d29653a905bc26f3c
GitHub账号 <3260191057@qq.com>
2022-10-08 17:06:53 +0800

    1

---
+++ src/components/charts/grade.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="grade">
+    <div ref="box" class="box"></div>
+    <div class="notFound" v-if="inNull">
+    <i class="iconfont icon_LC_icon_tips_fill"></i><span>该考生为参加考试</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"grade",
+    data(){
+      return {
+        isNull: false,
+        tableDataX:[],
+        tableDataY:[],
+      }
+    },
+    mounted(){
+      this.score();
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
