b84505ad339064929d0c8b7f9ff72029b6dd1aa4
GitHub账号 <3260191057@qq.com>
2022-10-10 11:42:50 +0800

    成绩统计

--- src/components/charts/grade.vue
+++ src/components/charts/grade.vue
@@ -20,9 +20,58 @@
     mounted(){
       this.score();
     },
-  }
+    methods:{
+      score(){
+        let studentId = this.$route.getStudentId
+        this.$axios('/api/score/${studentId}').then(res => {console.log(res)
+        if(res.data.code == 200){
+          let rootData = res.data.data
+          rootData.forEach((element,index) =>{
+            this.tableDataX.push('第${index + 1}')
+            this.tableDataY.push('element.etScore')
+          });
+          let boxDom = this.$refs["box"];
+          let scoreCharts = this.$echarts.init(boxDom);
+          let option = {
+            xAxis:{
+              type: "category",
+              data:this.tableDataX
+            },
+            yAxis:{
+              type: "value"
+            },
+            series:[
+              {
+                data:this.tableDataY,
+                type: "line",
+                itemStyle: {normal: {label:{show:true}}}
+              }
+            ]
+          };
+          scoreCharts.setOption(option);
+          scoreCharts.on("mouseover",params =>{
+            console.log(params.value);
+          });
+        }else{
+          this.inNull = true
+        }
+      })
+      }
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
-
+#grade{
+  position:relative;
+  .box {
+    width: 600px;
+    height: 400px;
+  }
+  .notFound{
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
 </style>
